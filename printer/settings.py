63ca1aefc030be20a8b10fd0d23d40d0adcc2774
JPWTCK <39982096+JPWTCK@users.noreply.github.com>
2025-09-17 16:58:49 +0100

    Configure WhiteNoise static file serving

--- printer/settings.py
+++ printer/settings.py
@@ -155,6 +155,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'whitenoise.runserver_nostatic',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
@@ -171,6 +172,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'  # For Crispy Forms
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -256,3 +258,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
