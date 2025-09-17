b0310a52ec22321cabae56d7baade2a670a023ad
JPWTCK <39982096+JPWTCK@users.noreply.github.com>
2025-09-17 11:24:21 +0100

    Load Django secret key from environment

--- printer/settings.py
+++ printer/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages_constants
+from django.core.management.utils import get_random_secret_key
 
 import os
 
@@ -31,7 +32,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wo8w8mup4ocubo%dglvr814!2jg#nj3y*1h0m%qdch=aj67rui'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+if not SECRET_KEY:
+    SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
